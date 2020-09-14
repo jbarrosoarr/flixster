e3689984b3a6dd47409ac0187c83e7237788498d
Jose Barroso <barroso.j52@gmail.com>
2020-09-13 22:56:33 -0700

    flix

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jose Barroso on 9/13/20.
+//  Copyright © 2020 jbarroso52. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
