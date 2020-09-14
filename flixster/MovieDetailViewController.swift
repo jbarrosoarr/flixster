0b8419ab7410c92973f95ee410a13adb07be800f
Jose Barroso <barroso.j52@gmail.com>
2020-09-14 01:56:38 -0700

    Flix app is complete

--- flixster/MovieDetailViewController.swift
+++ flixster/MovieDetailViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailViewController: UIViewController {
+    
+    var movie: [String:Any]!
 
     @IBOutlet weak var backdropImageView: UIImageView!
-    
     @IBOutlet weak var posterView: UIImageView!
-    
     @IBOutlet weak var titleLabel: UILabel!
-    
     @IBOutlet weak var sypnosisLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        sypnosisLabel.text = movie["overview"] as? String
+        sypnosisLabel.sizeToFit()
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)!
+        
+        
+        posterView.af_setImage(withURL: posterURL)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)!
+        
+        
+        backdropImageView.af_setImage(withURL: backdropURL)
 
         // Do any additional setup after loading the view.
     }
